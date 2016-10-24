a671d7882158ddeff676f53460b603062e39e2a1
Valdir Stumm Junior <stummjr@gmail.com>
2016-10-24 17:59:28 -0200

    add deploy files

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Automatically created by: shub deploy
+
+from setuptools import setup, find_packages
+
+setup(
+    name         = 'project',
+    version      = '1.0',
+    packages     = find_packages(),
+    package_data = {'price_monitor': ['resources/*.txt']},
+    entry_points = {'scrapy': ['settings = price_monitor.settings']},
+)
