6133f148ee4766105587a4139ca79028cf5ae6bf
Valdir Stumm Junior <stummjr@gmail.com>
2016-11-01 12:37:06 -0200

    enable auto throttling

--- price_monitor/settings.py
+++ price_monitor/settings.py
@@ -5,10 +5,7 @@ BOT_NAME = 'price_monitor'
 SPIDER_MODULES = ['price_monitor.spiders']
 NEWSPIDER_MODULE = 'price_monitor.spiders'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = 'price_monitor (+http://www.yourdomain.com)'
 ROBOTSTXT_OBEY = True
-DOWNLOAD_DELAY = 3
 
 SHUB_KEY = os.environ.get('$SHUB_KEY')
 
@@ -17,17 +14,5 @@ ITEM_PIPELINES = {
     'price_monitor.pipelines.CollectionStoragePipeline': 400,
 }
 
-# Enable or disable spider middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-# SPIDER_MIDDLEWARES = {
-#    'price_monitor.middlewares.MyCustomSpiderMiddleware': 543,
-# }
-
-# Enable or disable downloader middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'price_monitor.middlewares.MyCustomDownloaderMiddleware': 543,
-# }
-
-# AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # HTTPCACHE_ENABLED = True
