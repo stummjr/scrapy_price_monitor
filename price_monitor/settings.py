82927351d08d83d624e6a8274b1ab62e247e7a30
Valdir Stumm Junior <stummjr@gmail.com>
2016-11-07 12:04:44 -0200

    better document settings

--- price_monitor/settings.py
+++ price_monitor/settings.py
@@ -8,15 +8,15 @@ NEWSPIDER_MODULE = 'price_monitor.spiders'
 ROBOTSTXT_OBEY = True
 
 SHUB_KEY = os.getenv('$SHUB_KEY')
+# if you want to run it locally, replace '999999' by your Scrapy Cloud project ID below
+SHUB_PROJ_ID = os.getenv('SHUB_JOBKEY', '999999').split('/')[0]
+
 
 # settings for Amazon SES email service
 AWS_ACCESS_KEY = os.getenv('$AWS_ACCESS_KEY')
 AWS_SECRET_KEY = os.getenv('$AWS_SECRET_KEY')
-EMAIL_ALERT_FROM = 'Price Monitor <valdir@scrapinghub.com>'
-EMAIL_ALERT_TO = ['valdir@scrapinghub.com']
-
-
-SHUB_JOBKEY = os.getenv('SHUB_JOBKEY', '117761').split('/')[0]
+EMAIL_ALERT_FROM = 'Price Monitor <SENDER_EMAIL@provider.com>'
+EMAIL_ALERT_TO = ['RECEIVER_EMAIL@provider.com']
 
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
