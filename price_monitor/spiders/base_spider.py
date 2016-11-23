c112326457339b52bdc6475d9ea48d096d7a7fe6
Valdir Stumm Junior <stummjr@gmail.com>
2016-11-23 10:34:24 -0200

    small refactoring in base spider

--- price_monitor/spiders/base_spider.py
+++ price_monitor/spiders/base_spider.py
@@ -2,19 +2,15 @@ import json
 import pkgutil
 import scrapy
 from datetime import datetime
-from price_monitor.utils import get_product_names
 
 
 class BaseSpider(scrapy.Spider):
 
     def start_requests(self):
         products = json.loads(pkgutil.get_data('price_monitor', 'resources/urls.json').decode())
-        for product_name in get_product_names():
-            for url in products.get(product_name):
+        for name, urls in products.items():
+            for url in urls:
                 if self.name in url:
-                    item = {
-                        'product_name': product_name,
-                        'when': datetime.now().strftime('%Y/%m/%d %H:%M:%S'),
-                        'retailer': self.name,
-                    }
+                    now = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+                    item = {'product_name': name, 'retailer': self.name, 'when': now}
                     yield scrapy.Request(url, meta={'item': item})
